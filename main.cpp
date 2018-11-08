7e2b7107d1963ae662af87064ffc2f449a33c916
Kazikhan <41243203+Kazikhan13@users.noreply.github.com>
2018-11-08 19:22:41 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <array>
+using namespace std;
+
+
+
+
+int main()
+{
+	
+
+
+	return 0;
+}
+  
